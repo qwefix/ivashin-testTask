8b14ca0ecc72c73ee5d803bd12cbafc2895c8ef1
Hramchenlo Alexey <qwefix@gmail.com>
2021-08-31 18:55:09 +0300

    fix buttons

--- src/Ui components/Notes/Input/Input.jsx
+++ src/Ui components/Notes/Input/Input.jsx
@@ -5,34 +5,27 @@ function Input() {
     const inputRef = React.createRef()
     return (
         <div className={c.input_container + ' ' + c.active}>
+            
             <div className={c.text}>
-                <textarea className={c.textarea} 
+                <textarea className={c.textarea} disabled
                 onChange = {e=>console.log(inputRef.current.value)}
                 ref= {inputRef}
                 />
             </div>
-            <div className={`${c.accept} ${c.button}`}></div>
-            <div className={`${c.cancel} ${c.button}`}></div>
+
+            <div className={`${c.accept} ${c.button}`}>
+                Add Note
+            </div>
+            
+            <div className={`${c.cancel} ${c.button}`}>
+                Cancel
+            </div>
+
             <div className={c.tags_wrapper}>
                 <div className={c.tags}>
-                    <div className={c.tag}>#tag1</div>
-                    <div className={c.tag}>#tag2</div>
-                    <div className={c.tag}>#tag2</div>
-                    <div className={c.tag}>#tag2</div>
-                    <div className={c.tag}>#tag2</div>
-                    <div className={c.tag}>#tag2</div>
-                    <div className={c.tag}>#tag2</div>
-                    <div className={c.tag}>#tag1</div>
-                    <div className={c.tag}>#tag2</div>
-                    <div className={c.tag}>#tag2</div>
-                    <div className={c.tag}>#tag2</div>
-                    <div className={c.tag}>#tag2</div>
-                    <div className={c.tag}>#tag2</div>
                     <div className={c.tag}>#tag2</div>
                 </div>
             </div>
-            <div className={`${c.accept} ${c.button}`}></div>
-            <div className={`${c.cancel} ${c.button}`}></div>
         </div>
     )
 }
